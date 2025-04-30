dcfff475cbf629bfb0ffee61993142fc527ca8f2
mrjrklykn <omarjrkalayakan2000@gmail.com>
2025-04-30 07:41:21 +0000

    Task/PositiveNegativeOddEven.java

--- Task/PositiveNegativeOddEven.java
+++ Task/PositiveNegativeOddEven.java
@@ -1,15 +1,12 @@
 import java.util.Scanner;
-
 public class PositiveNegativeOddEven {
 
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+        Scanner nice = new Scanner(System.in);
         System.out.print("Enter a number: ");
 
-        // Read input correctly
-        int number = input.nextInt(); 
+        int number = nice.nextInt(); 
 
-        // Check if the number is positive, negative, or zero
         if (number < 0) {
             System.out.println(number + " is a negative number.");
         } else if (number > 0) {
@@ -17,8 +14,7 @@ public class PositiveNegativeOddEven {
         } else {
             System.out.println(number + " is zero.");
         }
-
-        // Check if the number is even or odd (excluding zero)
+     
         if (number != 0) {
             if (number % 2 == 0) {
                 System.out.println(number + " is an even number.");
@@ -26,7 +22,6 @@ public class PositiveNegativeOddEven {
                 System.out.println(number + " is an odd number.");
             }
         }
-
-        input.close(); // Close scanner to avoid resource leak
+        nice.close();
     }
 }
