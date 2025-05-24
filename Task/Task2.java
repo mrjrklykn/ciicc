0780a0f3069250921e4342a97617a43a9245197e
mrjrklykn <omarjrkalayakan2000@gmail.com>
2025-05-24 05:03:38 +0000

    Task/Task2.java  Task/Task3.java

--- Task/Task2.java
+++ Task/Task2.java
@@ -3,12 +3,12 @@ public class Task2
     public static void main(String[]args)
     {
         byte zero=0;
-        short one=1;
+        short one=1*1;
         int two= 3110;
         float y=2.0f;
         boolean x =true;
         char space =' ';
         String output="H"+two+space+"w"+zero+"r"+one+"d"+space+y+space+x;
-        System.out.println(output);
+        System.out.print(output);
     }
 }
